cc1f7e4fb3a66ab2e21952d964f5f6a4dd64a796
Jefferyyen <songpao226@gmail.com>
2025-05-16 19:34:31 +0800

    Update RWfromInternet.sh

--- RWfromInternet.sh
+++ RWfromInternet.sh
@@ -2,7 +2,7 @@
 # download_loop.sh  ── 每小時下載一次，完成後刪檔並計數
 
 URL="https://releases.ubuntu.com/noble/ubuntu-24.04.2-wsl-amd64.wsl"   # ← 換成你的連結
-DL_DIR="$(dirname "$0")"                 # ← 存檔夾
+DL_DIR="$HOME/Dowloads"                 # ← 存檔夾
 LOG="$HOME/ReadWriteFromInternet_$(date +%Y%m%d_%H%M%S).log"            # ← 日誌檔
 counter=0                                # 0. 初始計數
 
@@ -15,17 +15,17 @@ while true; do
     #### 2. 到連結下載 ##########################################
     wget -P "$DL_DIR" "$URL" | tee -a "$LOG"
     echo "$(date '+%F %T') 下載完成：$URL" | tee -a "$LOG"
-    ls /home/ubuntu/Downloads | tee -a "$LOG"
+    ls $HOME/Dowloads | tee -a "$LOG"
 
     #### 3. counter ++ #########################################
     counter=$((counter+1))
 
     #### 4. sleep 3600 #########################################
     echo "$(date '+%F %T') 休息 3600 秒…" | tee -a "$LOG"
-    sleep 3600
+    sleep 36
 
     #### 5. 刪掉下載檔案 ########################################
-    echo "$(date '+%F %T') 清理 /home/ubuntu/Downloads" | tee -a "$LOG"
-    rm -f /home/ubuntu/ubuntu-24.04.2-wsl-amd64.wsl 
+    echo "$(date '+%F %T') 清理 $HOME/Downloads" | tee -a "$LOG"
+    rm -f /$HOME/Dowloads/ubuntu-24.04.2-wsl-amd64.wsl
+    ls $HOME/Dowloads | tee -a "$LOG" 
 done
-
